88f76ac96920126e40620c32c2d0098f844c9197
GustavoGJesus <icarogabriel01@protonmail.com>
2021-12-07 14:37:26 -0300

    Checking if the dual user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,47 @@
+import { fauna } from "../../../services/fauna"
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
 
 export default NextAuth({
   // Configure one or more authentication providers
+  //Github OAuth
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-    // ...add more providers here
-  ],
-})
+   ],
+   callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      //Verificando se a duplo usuário em Collection
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email as string)
+                )
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email as string)
+              )
+            )
+          )
+        );
+        return true;
+      } catch {
+        return "/unauthorized";
+      }
+    },
+  },
+});
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,5 +1,8 @@
-import { Client } from 'faunadb'
+import { Client } from "faunadb";
 
 export const fauna = new Client({
-    secret: process.env.FAUNADB_KEY,
-})
+  secret: process.env.FAUNADB_KEY as string,
+  domain: "db.us.fauna.com",
+  port: 443,
+  scheme: "https",
+});
